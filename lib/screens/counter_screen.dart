bafd339fee66fc9773d408b194d5277a9f8acee6
Orlando Flores Silva <orlandofury@gmail.com>
2023-06-04 18:32:58 -0600

    Seccion 5: final de seccion

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -11,6 +11,19 @@ class _CounterScreenState extends State<CounterScreen> {
   //propiedades
   int counter = 10;
 
+  void increase(){
+    counter++;
+    setState(() {});
+  }
+  void reset(){
+    counter=0;
+    setState(() {});
+  }
+  void decrease(){
+    counter--;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     //variables
@@ -33,40 +46,45 @@ class _CounterScreenState extends State<CounterScreen> {
         ),
       ), 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_plus_1_outlined),
-              onPressed: () { 
-                  counter++;
-                  setState(() {
-                    
-                  });
-              },
-           ),
-           //const SizedBox(width: 20),
-           FloatingActionButton(
-            child: const Icon(Icons.restore),
-              onPressed: () { 
-                  counter=0;
-                  setState(() {
-                    
-                  });
-              },
-           ),
-           //const SizedBox(width: 20),
-           FloatingActionButton(
-            child: const Icon(Icons.exposure_minus_1_outlined),
-              onPressed: () { 
-                  counter--;
-                  setState(() {
-                    
-                  });
-              },
-           ),
-        ],
-      ),
+      floatingActionButton: CustomFloatingActions(increaseFn: increase,resetFn: reset,decreaseFn: decrease)
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+  final Function increaseFn;
+  final Function resetFn;
+  final Function decreaseFn;
+  
+  const CustomFloatingActions({
+    super.key, 
+    required this.increaseFn, 
+    required this.resetFn, 
+    required this.decreaseFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+         FloatingActionButton(
+           onPressed: () => increaseFn(), //() => setState(() => counter++)
+           child: const Icon(Icons.exposure_plus_1_outlined)
+         ),
+         //este SizedBox es util cuando se necesita precision al momento de agregar espacio a nuestros widgets
+         //const SizedBox(width: 20),
+         FloatingActionButton(
+          onPressed: () => resetFn(), //() => setState(() => counter=0)
+          child: const Icon(Icons.exposure_sharp),
+         ),
+         //const SizedBox(width: 20),
+         FloatingActionButton(
+          onPressed: () => decreaseFn(),// () => setState(() => counter--)
+          child: const Icon(Icons.exposure_minus_1_outlined)
+         ),
+      ],
     );
   }
 }
